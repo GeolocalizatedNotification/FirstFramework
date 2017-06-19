01976706e73d7b547a7ebada6715bc29e96f2aa7
Miguel Pimentel <miguelpimentel2013@gmail.com>
2017-06-19 14:40:09 -0300

    Adding gif option

--- Framework/LayoutPreferences.swift
+++ Framework/LayoutPreferences.swift
@@ -11,29 +11,32 @@ import UIKit
 
 public class LayoutPreferences: NSObject {
     
-    /**
-     
-    */
+   
+    // Set background color to status bar
     public static func setStatusBarColor(color: UIColor) {
         
         let statusBar: UIView = UIApplication.shared.value(forKey: "statusBar") as! UIView
         
         if statusBar.responds(to:#selector(setter: UIView.backgroundColor)) {
             statusBar.backgroundColor = color
+            
+
         }
     }
     
+    // Set status bar text color to black
     public static func setStatusBarTextColorToBlack() {
         
         UIApplication.shared.statusBarStyle = .default
     }
     
-    
+    // Set status bar text colot to white
     public static func setStatusBarTextColorToWhite() {
         
         UIApplication.shared.statusBarStyle = .lightContent
     }
     
+    // Set navigation bar background color, text colot e color for icons
     public static func navigationControllerProperties(color: UIColor, textColor: UIColor, iconColor: UIColor) {
         
         navigationControllerColor(color: color)
@@ -41,38 +44,58 @@ public class LayoutPreferences: NSObject {
         iconColorToNavbar(color: iconColor)
     }
     
-    
+    // Set navigation controller background color
     public static func navigationControllerColor(color: UIColor) {
         
         UINavigationBar.appearance().barTintColor = color
     }
     
+    
+    // Set text color in navbar
     public static func textColorToNavbar(color: UIColor) {
         
         UINavigationBar.appearance().titleTextAttributes =  [NSForegroundColorAttributeName: color]
     }
     
-    
+    // Set navbar icon color
     public static func iconColorToNavbar(color: UIColor) {
         
         UINavigationBar.appearance().tintColor = UIColor.white
     }
     
-    /**
-     Method that will hide the application's navigationBar
-     */
-    public static func hideNavigationBar(navigationController: UINavigationController){
+    // Hide navbarColor
+    public static func hideNavigationBar(navigationController: UINavigationController) {
+        
         navigationController.setNavigationBarHidden(true, animated: true)
     }
     
-    /**
-     Method that that will change the view's background color
-     */
-    public static func changeViewBackgroundColor(view: UIView, color: UIColor){
+   
+    // Set view background color
+    public static func changeViewBackgroundColor(view: UIView, color: UIColor) {
+        
         view.backgroundColor = color
     }
 }
 
+// Set possibility to hide keyboard when tapped around
+extension UIViewController {
+    
+    public func hideKeyboardWhenTappedAround() {
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    func dismissKeyboard() {
+    
+        view.endEditing(true)
+    }
+}
+
+
+
+// Set possibility to use hexadecimal colors in UIColor
 extension UIColor {
     convenience public init(red: Int, green: Int, blue: Int) {
         
